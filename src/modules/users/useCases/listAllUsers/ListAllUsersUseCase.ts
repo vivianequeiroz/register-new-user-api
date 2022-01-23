74e2f732faa0b7a9038cbac25d15533c6f7efa4f
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2022-01-23 16:36:40 -0300

    feat: forbidding a non existing user to list user

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -10,15 +10,21 @@ class ListAllUsersUseCase {
 
   execute({ user_id }: IRequest): User[] {
     const requestingUser = this.usersRepository.findById(user_id);
+    const nonExistingRequestingUser =
+      requestingUser === undefined || requestingUser === null;
+
+    if (nonExistingRequestingUser) {
+      throw new Error(
+        "Sorry, requesting user does not exist. Operation can not be completed."
+      );
+    }
 
     const isRequestingUserAdmin = requestingUser.admin === true;
-
     if (!isRequestingUserAdmin) {
       throw new Error("Only admin users can access this operation.");
     }
 
     const allUsers = this.usersRepository.list();
-
     return allUsers;
   }
 }
